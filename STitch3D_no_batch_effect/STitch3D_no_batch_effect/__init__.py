dded75dd902e9fe633dd3158a7b49a89849a9c90
nosignalmxh <maxiaoheng7@hotmail.com>
2024-12-15 15:14:08 +0800

    Initial commit without large files

---
+++ STitch3D_no_batch_effect/STitch3D_no_batch_effect/__init__.py
@@ -0,0 +1,4 @@
+import STitch3D_no_batch_effect.utils
+import STitch3D_no_batch_effect.model
+import STitch3D_no_batch_effect.networks
+import STitch3D_no_batch_effect.align_tools
---
+++ STitch3D_no_batch_effect/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stitch3d_no_batch_effect",
+    version="0.1",
+    packages=find_packages(),
+)
